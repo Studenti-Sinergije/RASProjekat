7e43cc6294d1910a853b203d984d65abe5e1815a
Safet55 <sajo_96@hotmail.com>
2019-10-20 15:17:55 +0200

    started working oNama page

--- src/oNama.php
+++ src/oNama.php
@@ -2,6 +2,36 @@
     require("includes/header.php");
 ?>
 
+<div>
+    <div>
+        <img src = "images\dule.jpg" alt = "Dusko Mirkovic">
+    </div>
+    
+    <div>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+</div>
+
+<div>
+    <div>
+        <img src = "images/stefo.jpg" alt = "Stefan Berovic">  
+    </div>
+    
+    <div>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+</div>
+
+<div>
+    <div>
+        <img src = "images/safi.jpg" alt = "Safet Omerbegic">
+    </div>
+    
+    <div>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+    </div>
+</div>
+
 <?php
     require("includes/footer.php");
 ?>
