3e5037641164316f15e4c6d4670c9c008874abf4
Safet55 <sajo_96@hotmail.com>
2019-10-28 20:00:08 +0100

    datebase

--- sql/datebase.sql
+++ sql/datebase.sql
@@ -1,34 +1,50 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.1.38-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win32
--- HeidiSQL Verzija:             10.2.0.5599
--- --------------------------------------------------------
+/*
+Navicat MySQL Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+Source Server         : ads
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : ras
 
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
 
--- Dumping database structure for ras
-CREATE DATABASE IF NOT EXISTS `ras` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `ras`;
+Date: 2019-10-28 19:58:58
+*/
 
--- Dumping structure for table ras.categories
-CREATE TABLE IF NOT EXISTS `categories` (
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for categories
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
+  `image` varchar(50) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
--- Dumping data for table ras.categories: ~0 rows (approximately)
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+-- ----------------------------
+-- Records of categories
+-- ----------------------------
+INSERT INTO `categories` VALUES ('1', 'Automobili', 'rsz_auto.jpg');
+INSERT INTO `categories` VALUES ('2', 'Nekretnine', 'rsz_1nekretnina.jpg');
+INSERT INTO `categories` VALUES ('3', 'Mobilni uredjaji', 'rsz_phone.jpg');
+INSERT INTO `categories` VALUES ('4', 'Kompjuteri', 'rsz_comp.jpg');
+INSERT INTO `categories` VALUES ('5', 'Tehnika', 'rsz_tehnika.jpg');
+INSERT INTO `categories` VALUES ('6', 'Poslovi', 'rsz_job.jpg');
+INSERT INTO `categories` VALUES ('7', 'Biznis', 'rsz_biznis.jpg');
+INSERT INTO `categories` VALUES ('8', 'Odjeca i obuca', 'rsz_ofinger.jpg');
+INSERT INTO `categories` VALUES ('9', 'Nakit', 'rsz_nakit.jpg');
+INSERT INTO `categories` VALUES ('10', 'Literatura', 'rsz_knjiga.jpg');
 
--- Dumping structure for table ras.oglas
-CREATE TABLE IF NOT EXISTS `oglas` (
+-- ----------------------------
+-- Table structure for oglas
+-- ----------------------------
+DROP TABLE IF EXISTS `oglas`;
+CREATE TABLE `oglas` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `cat_ID` int(10) unsigned NOT NULL,
   `creator_ID` int(10) unsigned NOT NULL,
@@ -47,12 +63,15 @@ CREATE TABLE IF NOT EXISTS `oglas` (
   CONSTRAINT `creator_ID` FOREIGN KEY (`creator_ID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table ras.oglas: ~0 rows (approximately)
-/*!40000 ALTER TABLE `oglas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `oglas` ENABLE KEYS */;
+-- ----------------------------
+-- Records of oglas
+-- ----------------------------
 
--- Dumping structure for table ras.users
-CREATE TABLE IF NOT EXISTS `users` (
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(50) NOT NULL,
   `password` varchar(300) NOT NULL,
@@ -63,10 +82,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table ras.users: ~0 rows (approximately)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+-- ----------------------------
+-- Records of users
+-- ----------------------------
