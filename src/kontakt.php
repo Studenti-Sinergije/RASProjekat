7c03557c14e08ad0b4d19d41e18014f25156202b
StefanBerovic <stefanberovic1@gmail.com>
2019-10-28 20:46:50 +0100

    added contact css and contact php

--- src/kontakt.php
+++ src/kontakt.php
@@ -5,30 +5,69 @@
 
 <div class="contact-form">
 <h2>Kontaktirajte nas</h2>
+
+<?php
+
+if ($_POST){
+
+	extract($_POST);
+	$message = '';
+
+	if ($ime === ''){
+		$message = 'Unesite ime!';
+	}else if ($email === ''){
+		$message = 'Unesite vašu e-mail adresu!';
+	}else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+		$message = 'Uneta e-mail adresa nije ispravna!';
+	}else if ($poruka === ''){
+		$message = 'Unesite poruku!';
+	}
+
+	if (!$message){
+		$to = 'besplatnioglasi@bo.ba'; 
+		$subject = 'Poruka sa sajta';
+		$email_message = $poruka;
+		$from = "$ime <$email>";
+		$headers =	'From: ' . $from . "\r\n" .
+				'Reply-To: ' . $from . "\r\n";
+
+		if (mail($to, $subject, $email_message, $headers)){ 
+			$message = 'Poruka je poslata!';
+		}else 
+			$message = 'Došlo je do greške, poruka nije poslata.';
+	}
+
+	print $message.'<br><br>';
+}
+
+?>
+
     <form method="post">
 	    <div>
 			Vaše ime:<br>
-                <input type="text" name="name">
+			<input name="ime" value="<?php echo $ime; ?>">	
 		</div>
 		<div>
 		    Vaša e-mail adresa:<br>
-                <input type="text" name="email">
+			<input name="email" value="<?php echo $email; ?>">
 		</div>
 		<div>
 			Poruka:<br>
-                <textarea name="comment" ></textarea>
+			<textarea rows="5" name="poruka"><?php echo $poruka; ?></textarea>
 		</div>
 		<div>
-            <input type="submit" name="submit" value="Pošalji"> 
+			<input class="button" type="submit" value=" Pošalji "> 
 		</div>
 	</form>
 </div>
+
 <div class="contact-info">
 <h2>Kontakt informacije</h3>
-    <p>*Neke info</p>
-    <p>*Neke info</p>
-    <p>*Neke info</p>
-    <p>*Neke info</p>
+	<ul>
+ 		<li>Neki tekst</li>
+ 		<li>Neki tekst</li>
+  		<li>Neki tekst</li>
+	</ul>
 </div>
 
 <?php
